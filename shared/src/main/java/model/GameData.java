10691db8a9bda841f4bfad36c5143ce59cd5c21e
stevehenny <stephen.henstrom@gmail.com>
2024-02-19 14:10:47 -0700

    UserData.java

---
+++ shared/src/main/java/model/GameData.java
@@ -0,0 +1,9 @@
+package model;
+import chess.ChessGame;
+
+public class GameData {
+    final int gameId;
+    final String gameName;
+    final ChessGame game;
+    final String whitePlayer;
+}
---
+++ shared/src/main/java/model/UserData.java
@@ -0,0 +1,66 @@
+package model;
+
+public class UserData {
+    private String username;
+    private String password;
+    private String email;
+
+    UserData(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserData{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserData userData = (UserData) o;
+
+        if (username != null ? !username.equals(userData.username) : userData.username != null) return false;
+        if (password != null ? !password.equals(userData.password) : userData.password != null) return false;
+        return email != null ? email.equals(userData.email) : userData.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username != null ? username.hashCode() : 0;
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
