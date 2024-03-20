8f02f0f5367280b391fc79831901a5aac5f63040
stevehenny <stephen.henstrom@gmail.com>
2024-03-20 17:25:24 -0600

    start board drawing

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -10,7 +10,6 @@ import static java.awt.Color.GREEN;
 import static ui.EscapeSequences.*;
 
 
-
 public class Repl {
     private final ChessClient client;
     private static final int BOARD_SIZE_IN_SQUARES = 8;
@@ -26,14 +25,12 @@ public class Repl {
     }
 
 
-    public void run(){
+    public void run() {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
         out.print(ERASE_SCREEN);
-        drawHeaders(out);
+        printWhitePerspective(out, new String[8][8]);
 
-        drawBoard(out);
-        //drawRowOfSquares(out);
 
         System.out.println("Welcome to Chess!");
         System.out.print(client.help());
@@ -60,125 +57,46 @@ public class Repl {
         System.out.print("\n" + ">>> ");
     }
 
-
-    private static void drawHeaders(PrintStream out) {
-
-        setBlack(out);
-
-        String[] headers = { "TIC", "TAC", "TOE", " ", "TIC", "TAC", "TOE", " " };
-        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-            drawHeader(out, headers[boardCol]);
-
-            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-                out.print(EMPTY.repeat(LINE_WIDTH_IN_CHARS));
-            }
+    public void printTopRow(PrintStream out) {
+        out.print(moveCursorToLocation(1, 1));
+        out.print("   ");
+        setGreySquare(out);
+        String spacing = "\u2001\u2005";
+        System.out.print(SET_BG_COLOR_LIGHT_GREY + EMPTY  + spacing + " ");
+        for (int i = 0; i < BOARD_SIZE_IN_SQUARES; i++) {
+            out.print(" " + (char) ('a' + i) + spacing);
         }
-
-        out.println();
+        out.print("\n");
     }
 
-    private static void drawHeader(PrintStream out, String headerText) {
-        int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
-        int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
-
-        out.print(EMPTY.repeat(prefixLength));
-        printHeaderText(out, headerText);
-        out.print(EMPTY.repeat(suffixLength));
-    }
-
-    private static void printHeaderText(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_BLACK);
-        out.print(SET_TEXT_COLOR_GREEN);
-
-        out.print(player);
-
-        setBlack(out);
-    }
-
-
-    private static void drawBoard(PrintStream out) {
-
-        for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
-
-            drawRowOfSquares(out);
-
-            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
-                drawVerticalLine(out);
-                setBlack(out);
-            }
-        }
-    }
-
-    private static void drawRowOfSquares(PrintStream out) {
-
-        for (int squareRow = 0; squareRow < SQUARE_SIZE_IN_CHARS; ++squareRow) {
-            for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-                setWhite(out);
-
-                if (squareRow == SQUARE_SIZE_IN_CHARS / 4) {
-                    int prefixLength = SQUARE_SIZE_IN_CHARS / 4;
-                    int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
-
-                    out.print(EMPTY.repeat(prefixLength));
-                    printPlayer(out, rand.nextBoolean() ? X : O);
-                    out.print(EMPTY.repeat(suffixLength));
+    public void printWhitePerspective(PrintStream out, String[][] board) {
+        printTopRow(out);
+        out.print(moveCursorToLocation(0,0));
+        String spacing = "\u2001\u2005\u2006";
+        String background = SET_BG_COLOR_LIGHT_GREY;
+        String[] rowLabels = {" 1"+spacing, " 2"+spacing, " 3"+spacing, " 4"+spacing, " 5"+spacing, " 6"+spacing, " 7"+spacing, " 8"+spacing};
+//        System.out.print(SET_BG_COLOR_BLACK + EMPTY);
+        for (int i = 0; i < BOARD_SIZE_IN_SQUARES; i++) {
+            out.print(SET_BG_COLOR_BLACK + EMPTY);
+            out.print(SET_BG_COLOR_LIGHT_GREY + rowLabels[i]);
+            for (int j = 0; j < BOARD_SIZE_IN_SQUARES; j++) {
+                if ((i + j) % 2 == 0) {
+                    out.print(SET_BG_COLOR_WHITE);
+                } else {
+                    out.print(SET_BG_COLOR_BLACK);
                 }
-                else {
-                    out.print(EMPTY.repeat(SQUARE_SIZE_IN_CHARS));
-                }
-
-                if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-                    // Draw right line
-                    setRed(out);
-                    out.print(EMPTY.repeat(LINE_WIDTH_IN_CHARS));
-                }
-
-                setBlack(out);
+                out.print(board[i][j]);
             }
-
-            out.println();
+            out.print(SET_BG_COLOR_LIGHT_GREY + rowLabels[i]);
+            out.print(SET_BG_COLOR_BLACK + EMPTY);
+            out.print("\n");
         }
     }
 
-    private static void drawVerticalLine(PrintStream out) {
-
-        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_CHARS +
-                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_CHARS;
-
-        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_CHARS; ++lineRow) {
-            setRed(out);
-            out.print(EMPTY.repeat(boardSizeInSpaces));
-
-            setBlack(out);
-            out.println();
-        }
-    }
-
-    private static void setWhite(PrintStream out) {
-        out.print(SET_BG_COLOR_WHITE);
-        out.print(SET_TEXT_COLOR_WHITE);
-    }
-
-    private static void setRed(PrintStream out) {
-        out.print(SET_BG_COLOR_RED);
-        out.print(SET_TEXT_COLOR_RED);
-    }
-
-    private static void setBlack(PrintStream out) {
-        out.print(SET_BG_COLOR_BLACK);
+    public void setGreySquare(PrintStream out) {
+        out.print(SET_BG_COLOR_LIGHT_GREY);
         out.print(SET_TEXT_COLOR_BLACK);
     }
-
-    private static void printPlayer(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_WHITE);
-        out.print(SET_TEXT_COLOR_BLACK);
-
-        out.print(player);
-
-        setWhite(out);
-    }
-
-
 
 
     public static void main(String[] args) {
